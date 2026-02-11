9ce9c39c04efba39464d59bf7dc79e03f220f7b9
Tokioshy <grizzlizyping@gmail.com>
2026-02-11 12:44:27 +0700

    Refining how interactions respond

--- commands/utility/specification.js
+++ commands/utility/specification.js
@@ -48,6 +48,6 @@ module.exports = {
             })
             .setTimestamp();
 
-        await interaction.reply({ embeds: [embed] });
+        await interaction.editReply({ embeds: [embed] });
     },
 };
