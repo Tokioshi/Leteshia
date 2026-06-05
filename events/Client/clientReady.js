84702f86a61e5a686011b6a290e1953717fd4de7
Tokioshi <grizzlizyping@gmail.com>
2026-06-05 09:48:57 +0700

    Adding event listener for exchange rate

--- events/Client/clientReady.js
+++ events/Client/clientReady.js
@@ -1,13 +1,24 @@
 const { Events } = require("discord.js");
 const chalk = require("chalk");
 const { initMusicPlayer } = require("../../utils/musicPlayer");
+const { updateLiveReport } = require("../../utils/getExchange");
 
 module.exports = {
     name: Events.ClientReady,
+    once: true,
     async execute(client) {
         console.log(
             chalk.magenta("[READY]"),
             chalk.white(`Connected as ${chalk.italic(client.user.tag)}!`),
+        );
+
+        await updateLiveReport(client);
+
+        setInterval(
+            () => {
+                updateLiveReport(client);
+            },
+            30 * 60 * 1000,
         );
 
         if (client.config.playLofi) {
