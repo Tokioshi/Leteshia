5df9033d8655a1d2b4f404c81ff8006082b0b4c0
Tokioshi <grizzlizyping@gmail.com>
2026-06-07 10:23:34 +0700

    Update exchange rate report schedule

--- events/Client/clientReady.js
+++ events/Client/clientReady.js
@@ -1,7 +1,8 @@
 const { Events } = require("discord.js");
-const chalk = require("chalk");
 const { initMusicPlayer } = require("../../utils/musicPlayer");
 const { updateLiveReport } = require("../../utils/getExchange");
+const chalk = require("chalk");
+const cron = require("node-cron");
 
 module.exports = {
     name: Events.ClientReady,
@@ -12,14 +13,28 @@ module.exports = {
             chalk.white(`Connected as ${chalk.italic(client.user.tag)}!`),
         );
 
-        await updateLiveReport(client);
+        let isUpdatingExchangeRate = false;
 
-        setInterval(
-            () => {
-                updateLiveReport(client);
-            },
-            30 * 60 * 1000,
-        );
+        const runExchangeRateUpdate = async () => {
+            if (isUpdatingExchangeRate) return;
+
+            isUpdatingExchangeRate = true;
+
+            try {
+                await updateLiveReport(client);
+            } catch (error) {
+                console.error("[ExchangeRate] Update failed:", error.message);
+            } finally {
+                isUpdatingExchangeRate = false;
+            }
+        };
+
+        await runExchangeRateUpdate();
+
+        cron.schedule("20 7 * * *", runExchangeRateUpdate, {
+            timezone: "Asia/Jakarta",
+            noOverlap: true,
+        });
 
         if (client.config.playLofi) {
             await initMusicPlayer(client);
